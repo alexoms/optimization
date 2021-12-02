ce922198b9363b9d2ed5c6cae407e1b9b37f2218
Alex Chang <alex.chang@unidatum.com>
2021-12-02 13:35:09 -0600

    nt

--- src/com/alex/optimization/problem/BankAccountProb.java
+++ src/com/alex/optimization/problem/BankAccountProb.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class BankAccountProb {
 
 	private static final long MEGABYTE = 1024L * 1024L;
-
-    public static long bytesToMegabytes(long bytes) {
+	
+	public static long bytesToMegabytes(long bytes) {
         return bytes / MEGABYTE;
     }
     
